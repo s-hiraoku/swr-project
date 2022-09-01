93b5d6bcdc8af62e49258fc3ea5776a58a5d9bef
hiraoku.shinichi <hiraoku.shinichi@MacBook-Pro-hiraoku.local>
2022-09-02 08:41:34 +0900

    first commit

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,11 @@
+import useSWR from "swr";
+
+export const useUser = (id) => {
+  const { data, error } = useSWR(`/api/user/${id}`, fetcher);
+
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
